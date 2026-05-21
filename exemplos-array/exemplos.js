65759c0595cb3cff5711a22ed71af06837e4eddd
enzomalatesta-jpg <maria.e.candido6@edu.senai.br>
2026-05-21 14:42:11 -0300

    Adicionando exemplos de array

--- exemplos-array/exemplos.js
+++ exemplos-array/exemplos.js
@@ -1,3 +1,44 @@
 //Maria Eduarda
 
-//Exemplos de arrey
+//Exemplos de array
+
+//Criando arrays com nomes de frutas e sucos
+let sucos = ["Laranja", "uva", "Abacaxi", "Manga", "Maracujá"];
+console.log(sucos[0]);
+
+//criando um array de números
+let numeros = [1,2,3,4,5];
+console.log(numeros[2]);
+
+//Acessando um elemento especifico do array
+let animais = ["Cachorro", "Gato", "Pássaro", "Peixe"];
+console.log(animais[1]);
+
+//Alternando um elemento do array
+let times = ["Flamengo", "Palmeiras", "São Paulo", "Corinthians"];
+times[2] = "Vasco";
+console.log(times);
+
+//Descobrindo o tamanho do array
+let games = ["FIFA", "Call of Duty", "Minecraft", "Fortnite"];
+console.log(games.length);
+
+//Adicionando um elemento ao final so array
+let cores = ["vermelho", "azul", "verde"];
+cores.push("amarelo");
+console.log(cores);
+
+//Removendo o ultimo elemento do array
+let carros = ["Fusca", "Gol", "Civic", "Corolla"];
+carros.pop();
+console.log(carros);
+
+//Removendo o primeiro elemento do array
+let cidades = ["São paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba"];
+cidades.shift();
+console.log(cidades);
+
+//Adicionando um elemento ni inicio do array
+let paises = ["Brasil", "Argentina", "Chile"];
+paises.unshift("Uruguai");
+console.log(paises);
